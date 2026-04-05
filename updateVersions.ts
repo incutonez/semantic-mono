6ec59065a40d827967a2710cbdc3c3dbea29dff2
Jef <incutonez@hotmail.com>
2026-04-05 17:20:19 -0400

    fix: Changing

--- updateVersions.ts
+++ updateVersions.ts
@@ -2,11 +2,12 @@
 import { existsSync } from "node:fs";
 
 const stdio = [0, 1, 2];
-const cwd = `packages/${process.argv[2]}`;
-if (!existsSync(cwd)) {
-	throw new Error("cwd is invalid");
-}
-execSync("npx semantic-release --deps.bump=inherit", {
-	stdio,
-	cwd,
-});
+console.log('here', process.argv[2])
+// const cwd = `packages/${process.argv[2]}`;
+// if (!existsSync(cwd)) {
+// 	throw new Error("cwd is invalid");
+// }
+// execSync("npx semantic-release --deps.bump=inherit", {
+// 	stdio,
+// 	cwd,
+// });
